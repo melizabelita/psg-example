7378b88a9681154c99e6a6ad21bd13450563062e
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-05-19 20:19:19 -0400

    1. Realizando un comentario y dibujando un cuadrado

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,9 @@
+# 1. Crea un archivo "ejercicio_01.py"
+# 2. Añade un comentario con la descripción del ejercicio "1"
+# 3. Crea un cuadrado con prints.
+
+print("...........")
+print(".         .")
+print(".         .")
+print(".         .")
+print("...........")
