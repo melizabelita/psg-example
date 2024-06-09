355fc9d89f12503d610f8472dc9c632b6465dae2
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-06-08 23:36:39 -0400

    Reto1. Cálculo Salario

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,19 @@
+# 1. Escribe un programa en Python que calcule el salario
+#    si la cantidad de horas trabajadas es 160 
+#    y la tarifa por hora de trabajo es 5.5 USD/hora.
+
+print("Horas trabajadas")
+
+# Definir las variables
+cantidad_hrs = 160
+tarifa_hrs = 5.5
+salario = 0
+
+# Calcular el salario
+salario = cantidad_hrs * tarifa_hrs
+
+# Imprimir el resultado
+print("Salario por hora 5.5 USD/hora")
+print("Horas trabajadas: 160")
+print("El salario es:")
+print(salario)
