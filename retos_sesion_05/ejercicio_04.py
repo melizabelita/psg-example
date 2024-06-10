8f84b80a35b3c035bc9e82699470a83d69b7db8f
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-06-09 22:12:26 -0400

    Reto4. Volumen de un cubo

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,13 @@
+# 4. Calcular el volumen de un cubo donde uno de sus lados mide 10 metros,
+# utilizando la fórmula general del volumen de un cubo
+# V = a^3
+
+# Longitud de los lados del cubo en metros
+lado = 10
+
+# Calcular el volumen del cubo usando la fórmula de volumen V = a^3
+volumen = lado ** 3
+
+# $l resultado
+print("El volumen del cubo cuyo lado es 10 metros es:")
+print(volumen)
