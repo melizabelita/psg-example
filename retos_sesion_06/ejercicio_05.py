10ee32ddb582cde26e100bc720e031cd689661e2
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-07-19 23:35:01 -0400

    Ejercicio 5 - Paridad?

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,8 @@
+print("Comparar los números 123 y 890, comprobar si tienen la misma paridad ambos pares o ambos impares")
+nro1 = 123
+nro2 = 890
+
+misma_paridad = (nro1 % 2) == (nro2 % 2)
+
+print(nro1, nro2, "¿Tienen la misma paridad?")
+print(misma_paridad)
