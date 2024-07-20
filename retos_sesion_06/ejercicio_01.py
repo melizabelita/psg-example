0597352ecc511e7357621a4b49fc842f5f572377
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-07-19 22:14:24 -0400

    Ejercicio 1 - -1 es True?

---
+++ retos_sesion_06/ejercicio_01.py
@@ -0,0 +1,6 @@
+print("¿Si se declara una variable boolean del valor -1 es True o False?")
+variable = -1
+print(bool(variable)) 
+print()
+print("cualquier valor distinto de cero se evalúa como True")
+print("cualquier valor igual a cero se evalúa como False")
