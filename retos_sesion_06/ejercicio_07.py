17f17016203cb23de2feaf6dee3b8693d1d3611e
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-07-19 23:50:27 -0400

    Ejercicio 7 - Son iguales?

---
+++ retos_sesion_06/ejercicio_07.py
@@ -0,0 +1,6 @@
+print("¿El cuadrado y la suma de 2 y 2 son iguales?")
+suma = 2 + 2
+print("La suma es: ", suma)
+cuadrado = 2 * 2
+print("El cuadrado es: ", cuadrado)
+print("El cuadrado y la suma son iguales? ", suma == cuadrado)
