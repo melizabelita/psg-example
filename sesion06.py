f42614ea44c040184f0db82451fab60ef8bb96d2
Elizabeth Illatarco <melizabelita@hotmail.com>
2024-07-19 21:42:16 -0400

    Clase sesión 06

---
+++ sesion06.py
@@ -0,0 +1,191 @@
+print ("Tipos de datos booleanos")
+print (True)
+print (False)
+print (True + True)
+print (True * True)
+print (True * False)
+print (False + False)
+print (False * False)
+print (type(True * True))
+
+print("")
+print ("Números y booleanos")
+print (10 + True)
+print (10 + False)
+print (10 * True)
+print (10 * False)
+
+print("")
+print ("Declarar variables booleanas")
+var_booleana = True
+print (var_booleana)
+print (type(var_booleana))
+var_booleana = False
+print (var_booleana)
+print (type(var_booleana))
+
+print("")
+print ("Declarar mediante función bool()")
+var_booleana = bool(1)
+print("var_booleana = bool(1)")
+print (var_booleana)
+print (type(var_booleana))
+print("var_booleana = bool(0)")
+var_booleana = bool(0)
+print (var_booleana)
+print (type(var_booleana))
+print("var_booleana = bool(15)")
+var_booleana = bool(15)
+print (var_booleana)
+print (type(var_booleana))
+
+print("")
+print ("Operadores de comparación")
+print("A nivel de magnitud")
+print("print (10 == 10)")
+print (10 == 10)
+print("print (10 != 10)")
+print (10 != 10)
+print("print (10 < 10)")
+print (10 < 10)
+print("print (10 > 10)")
+print (10 > 10)
+print("print (10 <= 10)")
+print (10 <= 10)
+print("print (10 >= 10)")
+print (10 >= 10)
+print("A nivel de objeto")
+print("print (10 is 10)")
+print (10 is 10)
+print("print (10 is not 10)")
+print (10 is not 10)
+print("print (10 is 10.0)")
+print (10 is 10.0)
+print("print (10 is not 10.0)")
+print (10 is not 10.0)
+
+print("")
+print ("Asignación de variables")
+x = 10
+mayor_que_cero = x > 0
+print("print (mayor_que_cero)")
+print (mayor_que_cero)
+diferente_de_10 = x != 10
+print("print (diferente_de_10 = x != 10)")
+print (diferente_de_10)
+
+print()
+print ("Operadores lógicos")
+print (True and True)
+print (True and False)
+print (False or True)
+print (False or False)
+print (not True)
+print (not False)
+
+print()
+print ("Operadores lógicos y prioridad")
+print (False and False or True)
+print (False and (False or True))
+print (not True and False or True)
+print (not (True and False or False))
+print (not True and (False or False))
+print (not True and False or False)
+
+print()
+print ("Operador AND")
+print (True and True)
+print (True and False)
+print (False and True)
+print (False and False)
+
+print()
+print ("Operador OR")
+print (True or True)
+print (True or False)
+print (False or True)
+print (False or False)
+
+print()
+print ("Operador NOT")
+print (not True)
+print (not False)
+
+print()
+print ("Operador NAND")
+print (not (True and True))
+print (not (True and False))
+print (not (False and True))
+print (not (False and False))
+
+print()
+print ("Operador NOR")
+print (not (True or True))
+print (not (True or False))
+print (not (False or True))
+print (not (False or False))
+
+print()
+print ("Operador XOR")
+a = True
+b = False
+print ((a or b) and not (a and b))
+a = True
+b = True
+print ((a or b) and not (a and b))
+
+print()
+print ("Ejemplo de uso Sensor y Batería")
+sensor = True
+bateria = True
+print (sensor, "and", bateria, "=", sensor and bateria)
+sensor = True
+bateria = False
+print (sensor, "and", bateria, "=", sensor and bateria)
+sensor = False
+bateria = True
+print (sensor, "and", bateria, "=", sensor and bateria)
+sensor = False
+bateria = False
+print (sensor, "and", bateria, "=", sensor and bateria)
+
+print()
+print ("Ejemplo 1 - Comparación y Lógicos")
+numero = 20
+print (numero >= 0 and numero <= 100)
+
+print()
+print ("Ejemplo 2 - Aritméticos y comparación")
+nota1 = 15
+nota2 = 20
+nota3 = 16
+print("Nota1", nota1)
+print("Nota2", nota2)
+print("Nota3", nota3)
+print("Nota Final", nota1 + nota2 + nota3)
+print ((nota1 + nota2 + nota3) > 50)
+
+print()
+print ("Ejemplo 3 - Aritméticos, comparación y lógicos")
+numero = 15
+print("Determinar si el número 15 es divisible por 3 y 5, pero no por 2")
+print (numero % 3 ==0)
+print ("Es divisible entre 3?", numero % 3 ==0)
+print ("Es divisible entre 5?", numero % 5 ==0)
+print ("Es divisible entre 2?", numero % 2 ==0)
+print ("No es divisible entre 2?", numero % 2 !=0)
+print ((numero % 3 ==0) and (numero % 5 ==0) and (numero % 2 !=0))
+
+print()
+print ("Cortocircuito con operador and")
+x = 1
+y = 0
+print (x > 2 and (x/y) > 2)
+#print (x > 0 and (x/y) > 0)
+
+print()
+print ("Cortocircuito con operador or")
+x = 1
+y = 0
+print (x > 0 or (x/y) > 0)
+#print (x > 2 or (x/y) > 2)
